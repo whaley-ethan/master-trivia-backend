eee5029f4844b72ff279fa5d0c213fcb2ceca6bd
Ethan Whaley <51497042+solidlikeplato@users.noreply.github.com>
2020-01-28 16:39:30 -0600

    added in some back end statistics calculations

---
+++ faker.py
@@ -0,0 +1,10 @@
+from api.models import User, Answer
+from users.models immport CustomUser
+from faker import Faker
+
+
+class UserFaker:
+    self.faker = Faker()
+
+    def MakeFakeUser(self):
+        
--- users/serializers.py
+++ users/serializers.py
@@ -1,6 +1,8 @@
-from rest_framework import serializers
-from . import models
 from api.models import Answer, Quiz
+from django.db.models import Count
+from rest_framework import serializers
+
+from . import models
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
@@ -8,22 +10,34 @@ class UserSerializer(serializers.ModelSerializer):
         fields = ('id','email', 'username', )
 
 class UserStaticsicsSerializer(serializers.ModelSerializer):
-    top_five_times = serializers.SerializerMethodField()
-    top_five_categories = serializers.SerializerMethodField()
+    five_fastest_right_answers = serializers.SerializerMethodField()
+    five_best_categories = serializers.SerializerMethodField()
+    favorite_category = serializers.SerializerMethodField()
 
     class Meta:
         model = models.CustomUser
-        read_only_fields = ('top_five_times',)
-        fields = ('id', 'username','top_five_times', 'top_five_categories', )
+        read_only_fields = ('five_fastest_right_answers',)
+        fields = ('id', 'username','five_fastest_right_answers', 'favorite_category', 'five_best_categories')
     
-    def get_top_five_times(self, obj):
-        all_user_quizes = [quiz.id for quiz in obj.quizes.all()]
-        up_to_five_user_answers = Answer.objects.filter(quiz__in=all_user_quizes).order_by('time')[:5].all()
-        up_to_five_times = [answer.time for answer in up_to_five_user_answers]
-        return up_to_five_times
-    
-    def get_top_five_categories(self, obj):
-        all_user_quizes = [quiz.id for quiz in obj.quizes.all()]
-        up_to_five_user_answers = Answer.objects.filter(quiz__in=all_user_quizes).order_by('time')[:5].all()
-        up_to_five_categories = [answer.category for answer in up_to_five_user_answers]
-        return up_to_five_categories
+    def get_all_user_quizes(self, obj):
+        return [quiz.id for quiz in obj.quizes.all()]
+
+    def get_category_counts(self, obj):
+        all_user_quizes = self.get_all_user_quizes(obj)
+        user_categories = Answer.objects.filter(quiz__in=all_user_quizes).values("category").annotate(Count("id"))
+        return user_categories
+
+    def get_five_fastest_right_answers(self, obj):
+        all_user_quizes = self.get_all_user_quizes(obj)
+        up_to_five_right_answers = Answer.objects.filter(quiz__in=all_user_quizes, didGetRight=True).order_by('time')[:5].all()
+        up_to_five = [{'category': answer.category, 'time': answer.time} for answer in up_to_five_right_answers]
+        return up_to_five
+
+    def get_favorite_category(self, obj):
+        return self.get_category_counts(obj)[:1]
+
+    def get_five_best_categories(self, obj):
+        return 42
+        # filter answers to those associated with this user through quizes.
+        # sort by count by unique category, where didGetRight == True / (count by unique category) limit 5
+        # return object {answer.key : answer.calculated_time for answer in filtered list }
